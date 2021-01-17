3af84d58dd5b6840c58071f3b4e8479d69b243f1
miamagana <miguelangelmagana27@gmail.com>
2021-01-17 16:11:59 +0100

    Main functionality implemented

--- shopping_cart/init_mongo.js
+++ shopping_cart/init_mongo.js
@@ -1,31 +1,35 @@
-const mongoclient = require('mongodb').MongoClient;
-var assert = require('assert');
+const mongoclient = require("mongodb").MongoClient;
+const assert = require("assert");
 
-var url='mongodb://mongodb:27017';
+const url = "mongodb://mongodb:27017";
 
 const insertDocuments = function (db, callback) {
   // Get the documents collection
-  var collection = db.collection('products');
+  const collection = db.collection("products");
   // Insert some documents
-  collection.insertMany([
-    {cod : 1,desc:'palos',stock:0}, {cod : 2,desc:'hierros',stock:10}, {cod : 3,desc:'muelles',stock:5}
-  ], function(err, result) {
-    assert.strictEqual(err, null);
-    assert.strictEqual(3, result.result.n);
-    assert.strictEqual(3, result.ops.length);
-    console.log("Inserted 3 products into the collection");
-    callback(result);
-  });
-}
+  collection.insertMany(
+    [
+      { cod: 1, desc: "palos", stock: 0 },
+      { cod: 2, desc: "hierros", stock: 10 },
+      { cod: 3, desc: "muelles", stock: 5 },
+    ],
+    function (err, result) {
+      assert.strictEqual(err, null);
+      assert.strictEqual(3, result.result.n);
+      assert.strictEqual(3, result.ops.length);
+      console.log("Inserted 3 products into the collection");
+      callback(result);
+    }
+  );
+};
 
-mongoclient.connect(url, function(err, client) {
-  assert.strictEqual(null, err);
+mongoclient.connect(url, function (err, client) {
+  strictEqual(null, err);
   console.log("Connected successfully to server");
-  const db = client.db('almacen');
-  insertDocuments(db,function() {
-        	if (err) throw err;
-        	console.log('success with insertion!');
-          client.close();
+  const db = client.db("almacen");
+  insertDocuments(db, function () {
+    if (err) throw err;
+    console.log("success with insertion!");
+    client.close();
   });
 });
-
---
+++ shopping_cart/shopping_cart.js
@@ -0,0 +1,94 @@
+const mongoclient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = "mongodb://localhost:27017";
+
+mongoclient.connect(url, (err, client) => {
+  assert.strictEqual(err, null);
+  console.log("Connected to DB");
+  const db = client.db("almacen");
+  addToCart(db, "muelles", 2);
+  addToCart(db, "hierros", 2);
+  addToCart(db, "maderas", 2);
+  setTimeout(function () {
+    removeFromCart("muelles", 1);
+    removeFromCart("hierros", 2);
+    removeFromCart("hierros", 2);
+  }, 500);
+});
+
+const shopping_cart = [];
+
+/* 
+    Function to add an element from the cart 
+    If the element does not exist in the DB it warns the user
+*/
+const addToCart = (db, description, quantity) => {
+  return new Promise((resolve) => {
+    const collection = db.collection("products");
+    findProduct(description, collection)
+      .then((element) => {
+        const productInCart = shopping_cart.find(
+          (prod) => prod.desc === description
+        );
+        if (!!productInCart) {
+          productInCart.quantity += quantity;
+        } else {
+          const { cod, desc } = element;
+          const product = { cod, desc, quantity };
+          shopping_cart.push(product);
+        }
+        resolve(logCart());
+      })
+      .catch((err) => console.warn(`Product ${description} does not exist`));
+  });
+};
+
+/* 
+    Function to remove an element from the cart 
+    If the quantity of the product to remove is greater or equal the current quantity it 
+    removes the element from the cart entirely, otherwise just substracts the quantity for the cart.
+*/
+const removeFromCart = (description, quantity) => {
+  return new Promise((resolve) => {
+    const productInCart = shopping_cart.find(
+      (prod) => prod.desc === description
+    );
+    if (productInCart) {
+      const newQuantity = productInCart.quantity - quantity;
+      if (newQuantity <= 0) {
+        const index = shopping_cart.findIndex(
+          (prod) => prod.desc === description
+        );
+        shopping_cart.splice(index, 1);
+      } else {
+        productInCart.quantity = newQuantity;
+      }
+      return resolve(logCart());
+    } else {
+      console.log(`Product ${description} is not in the cart`);
+    }
+  });
+};
+
+const findProduct = (desc, collection) => {
+  return new Promise((resolve, reject) => {
+    collection.findOne({ desc }, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(res);
+    });
+  });
+};
+
+function logCart() {
+  console.log(`Your cart: ${JSON.stringify(shopping_cart)}`);
+}
+
+// Activity 2
+function logNoStock(product) {
+  console.log(
+    `Request can not be processed due to lack of stock for product ${product}`
+  );
+}
